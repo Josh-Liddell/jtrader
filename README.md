36ac8ef37a38f9655150ac88accf20613ea7c06f
Joshua Liddell <joshua.liddell21@gmail.com>
2025-04-17 13:40:30 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# JTrader
+JTrader is a command-line tool for simulating and monitoring triangular arbitrage opportunities in the crypto market. 
+It lets you analyze token paths, snapshot exchange rates, and run background watchers to identify profitable cycles between crypto pairs.
+
+
+# Installation
+
+
+# Usage
---
+++ jtrader.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import argparse as ap
+import yaml
+# from customparser import CustomArgParser
+# from tradestrategies import MeanReversion
+
+
+class JTrader():
+
+    # Create the argument parser
+    def __init__(self, config):
+        self.parser = ap.ArgumentParser(
+            description='A tool for simulating crypto arbitrage opportunities.Use it to start a background watcher that checks for arbitrage paths',
+            # usage='jtrader command [options]',
+            epilog='Have fun trading!',
+            add_help=False,
+            allow_abbrev=False)
+
+        self._config_parsers(config)
+
+    # Applying configuration to the parser object
+    def _config_parsers(self, config):
+        # Apply general argument options
+        for args in config['general']:
+            flags = args.pop('flags')
+            self.parser.add_argument(*flags, **args)
+
+        # Create subparsers and apply their argument options
+        subparsers = self.parser.add_subparsers(dest='command', title='subcommands')
+        commands = config["commands"]
+
+        for command, data in commands.items():
+            subparser = subparsers.add_parser(command, help=data["help"])
+            subparser.set_defaults(func=getattr(self, data['func']))
+
+            for args in data["args"]:
+                flags = args.pop("flags")
+                subparser.add_argument(*flags, **args)
+
+    # Logic control for arugments
+    def run(self):
+        args = self.parser.parse_args()
+
+        # If a subcommand was entered run its assocated function
+        if args.command:
+            args.func(args)
+        else:
+            self.parser.print_help()
+
+    def start(self, args):
+        print("Program started!")
+        print(args.capital)
+
+    def stop(self, args):
+        print("Program stopping")
+
+
+# This code could get transfered to a main or init file later
+if __name__ == '__main__':
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    jtrader = JTrader(config)
+    jtrader.run()
+
